0ed21525281e9e25ebb77293c63d93564747c7f2
Unknown <marcohwlam@hotmail.com>
2018-07-14 02:47:32 -0400

    kemptysort

--- src/KEmptySlots.java
+++ src/KEmptySlots.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+/*
+ * Created by Ho Wang Lam
+ * marcohwlam@hotmail.com
+ * Copyright (c) Seamless Compute 2018.
+ */
 
 public class KEmptySlots {
+    public int kEmptySlots(int[] flowers, int k) {
+        int[] days = new int[flowers.length];
+        for (int i = 0; i < flowers.length; i++) days[flowers[i] - 1] = i + 1;
+        int left = 0, right = k + 1, res = Integer.MAX_VALUE;
+        for (int i = 0; right < days.length; i++) {
+            if (days[i] < days[left] || days[i] <= days[right]) {
+                if (i == right) res = Math.min(res, Math.max(days[left], days[right]));   //we get a valid subarray
+                left = i;
+                right = k + 1 + i;
+            }
+        }
+        return (res == Integer.MAX_VALUE) ? -1 : res;
+    }`
 }
--- src/NextClockTime.java
+++ src/NextClockTime.java
@@ -12,13 +12,13 @@ import java.util.Set;
 
 public class NextClockTime {
 
-
     private Set<String> permutationSet = new HashSet<>();
-    private void permute(String time, int l, int r){
-        if (l==r) permutationSet.add(time);
+
+    private void permute(String time, int l, int r) {
+        if (l == r && isValidTime(time)) permutationSet.add(time);
         for (int i = l; i <= r; i++) {
             time = swap(time, l, i);
-            permute(time, l+1, r);
+            permute(time, l + 1, r);
             time = swap(time, l, i);
         }
     }
@@ -27,43 +27,34 @@ public class NextClockTime {
         return permutationSet;
     }
 
-    private String swap(String a, int i, int j)
-    {
+    private String swap(String a, int i, int j) {
         char temp;
         char[] charArray = a.toCharArray();
-        temp = charArray[i] ;
+        temp = charArray[i];
         charArray[i] = charArray[j];
         charArray[j] = temp;
         return String.valueOf(charArray);
     }
 
-    public String nextClockTime(String time){
-        time = time.replace(":","");
-        permute(time, 0, time.length()-1);
+    public String nextClockTime(String time) {
+        //remove ":"
+        time = time.replace(":", "");
+        //Create Set of permutation in permutationSet
+        permute(time, 0, time.length() - 1);
         List<String> permutation = new ArrayList<>(permutationSet);
         Collections.sort(permutation);
-        //find the next larger valid permutation
-
         //set index to current time
         int index = permutation.indexOf(time);
         //return the first permutation of the next day if there is not bigger permutation in today
-        if (index == permutation.size()-1){
+        if (index == permutation.size() - 1) {
             return permutation.get(0);
         }
-        //move pointer to the next clock time
-        index++;
-        while ( index <=permutation.size()) {
-            if(isValidTime(permutation.get(index))) {
-                return new StringBuilder(permutation.get(index)).insert(2,":").toString();
-            }
-            //roll to next day at the end of array
-            index = (index == permutation.size()-1)? 0 : index+1;
-        }
-        return new StringBuilder(time).insert(2,":").toString();
+        //move index to the next larger valid permutation
+        return new StringBuilder(permutation.get(index + 1)).insert(2, ":").toString();
     }
 
-    private boolean isValidTime(String time){
-        return ( (time.charAt(0)<='2' && time.charAt(1)<='3')
-                || (time.charAt(0)<='1'&&time.charAt(2)<='9'&& time.charAt(3)<='5'));
+    private boolean isValidTime(String time) {
+        return ((time.charAt(0) <= '2' && time.charAt(1) <= '3')
+                || (time.charAt(0) <= '1' && time.charAt(2) <= '9' && time.charAt(3) <= '5'));
     }
 }
