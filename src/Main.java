3a4a509352b81e336da59fcb8382f03602993127
Unknown <marcohwlam@hotmail.com>
2018-09-28 23:10:41 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 7fe3afdedfca3f1d3a8bd393add27fe688dfef0b.

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        GameOfLife G = new GameOfLife();
-        int[][] in = new int[][] {
-                {0,1,0},
-                {0,1,0},
-                {1,1,1},
-                {0,0,0}
-        };
-        G.gameOfLife(in);
+//        KEmptySlots M = new KEmptySlots();
+//        int[] inoput= {1, 2, 7, 6, 4, 3, 5};
+//        System.out.println(M.kEmptySlots(inoput, 1));
+        ValidParentheses V = new ValidParentheses();
+            String in = "()";
+            V.isValid(in);
+//         a = C.champagneTower(0,1,0);
+//        System.out.println(a);
+//            a = C.champagneTower(2,1,1);
+//        System.out.println(a);
+
+
     }
 }
